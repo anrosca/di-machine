7b36cf64890fa5bf353ffe80c3a927027c2ced0b
Andrei Rosca <Andrei.Rosca@endava.com>
2020-11-26 15:19:24 +0200

    Meta-annotated the @Service & @Configuration annotations with @Component

--- src/main/java/com/dimachine/core/annotation/Service.java
+++ src/main/java/com/dimachine/core/annotation/Service.java
@@ -6,6 +6,7 @@ import java.lang.annotation.*;
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
+@Component
 public @interface Service {
     String value() default "";
 }
